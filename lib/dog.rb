cf1796073d2aa68cd328928b159d6c8fd7f552b4
Shreyans Jain <gamingeyefinity@gmail.com>
2017-02-13 09:09:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+class Dog
+
+    def name=(name)
+        @name = name
+    end
+
+
+    def name
+        return @name
+    end
+
+
+    def bark
+        puts "woof!"
+    end
+
+end
+
+
+#testing area
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+# => "Fido"
+
+fido.bark
+# woof!
+# => nil
