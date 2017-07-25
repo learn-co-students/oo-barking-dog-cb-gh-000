65cf81e515c9ee02ab700a515227fb5fbd2d3f5c
Akram Taha <akram.eltranesy95@eng-st.cu.edu.eg>
2017-07-25 21:19:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Dog
+  def name(name)
+    @name=name
+  end
+  def name
+    @name
+  end
+  def name=newName
+    @name=newName
+  end
+
+  def bark(bark)
+    @bark=bark
+  end
+  def bark
+    puts "woof!"
+  end
+  def bark=bark
+    @bark=bark
+  end
+end
