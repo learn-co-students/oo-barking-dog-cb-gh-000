a4b78b2794d098af7b7b4f086e11799b1f166bdc
Abhijit Roy <aroy30@uic.edu>
2019-01-19 20:31:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+
+  # setter
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  # getter
+  def name
+    @name
+  end
+
+  # bark
+  def bark
+    puts "woof!"
+  end
+
+end
