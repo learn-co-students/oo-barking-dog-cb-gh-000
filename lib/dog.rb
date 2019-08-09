a1d7c86545b58b13376adb37f3f243f7a77884f3
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-09 22:50:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog
+  def initialize (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name = (new_name)
+    @name = new name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+fido.bark
