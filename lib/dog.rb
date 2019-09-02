c8a9b916cb512b607d8e49b5b6bd7443e2282756
Edgar V <evenegas9@ucmerced.edu>
2019-09-02 04:23:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog
+  def name=(name)
+    # setting/adding property/variable to the instance
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
